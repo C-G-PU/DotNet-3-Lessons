50fec2a9c0149b8358e8670491d60eaaeff2de20
C-G-PU <61973453+C-G-PU@users.noreply.github.com>
2020-11-29 19:34:13 +0300

    TaskApp

---
+++ TaskApp/TaskApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace TaskApp
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int a = 0;
+            Task task = new Task(() =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    a++;
+                }
+            });
+            task.Start();
+            Thread.Sleep(400);
+            
+            Console.WriteLine(a);
+            Console.ReadLine();
+        }
+    }
+}
